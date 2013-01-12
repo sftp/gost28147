009e92a5baa793d014db62fb8defd1263e2f8cb3
Sergey V <sftp.mtuci@gmail.com>
2013-01-13 00:50:11 +0400

    files.h: fix typo: 'sourse' -> 'source'

--- files.h
+++ files.h
@@ -7,7 +7,7 @@ void ecb_crypt_file(FILE *src, FILE *dst, u32 *key, u64 size, u8 encrypt)
 	u32 *buffer = malloc(BUFF_SIZE);
 
 	if (size % 8 != 0) {
-		printf("In ECB mode sourse file size should be multiple of 8\n");
+		printf("In ECB mode source file size should be multiple of 8\n");
 		exit(-1);
 	}
 	
